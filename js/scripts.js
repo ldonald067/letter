c3a21be324d6f33caec3ba9fd2908d4e06b59393
LaTanya Donaldson and Zach Bastian <latanya@>
2017-03-22 10:47:20 -0700

    it will add uppercase to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event){
-    var person1Input = $("input#person1").val();
+    var person1Input = $("input#person1").val().toUpperCase();
 
     $(".person1").text(person1Input);
 
