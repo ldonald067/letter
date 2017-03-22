efe7bfc8f413576b736d80b714b295aad01be1bf
LaTanya Donaldson and Zach Bastian <latanya@>
2017-03-22 09:53:15 -0700

    it will add two containers, js & css elements.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+    var person1Input = $("input#person1").val();
+
+    $(".person1").text(person1Input);
+
+    $("#letter").show();
+
+    event.preventDefault();
+  });
+});
